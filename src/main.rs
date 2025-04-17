3138e1a14128f0be2e54c4fec2132023ef0f0000
David Gilmore <dgilmore@magnite.com>
2025-04-17 11:52:19 +0100

    obj3

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,56 @@ fn encode_bytes(bytes: &mut Vec<u8>) {
     }
 }
 
+fn base64_encode<T: AsRef<[u8]>>(input: T) -> String {
+    const BASE64_CHARS: &[u8; 64] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+    let input = input.as_ref();
+    let mut output = String::new();
+
+    let mut i = 0;
+    while i < input.len() {
+        let b1 = input[i];
+        let b2 = if i + 1 < input.len() { input[i + 1] } else { 0 };
+        let b3 = if i + 2 < input.len() { input[i + 2] } else { 0 };
+
+        // Encode 6-bit groups
+        output.push(BASE64_CHARS[(b1 >> 2) as usize] as char);
+        output.push(BASE64_CHARS[(((b1 & 0b0000_0011) << 4) | (b2 >> 4)) as usize] as char);
+        if i + 1 < input.len() {
+            output.push(BASE64_CHARS[(((b2 & 0b0000_1111) << 2) | (b3 >> 6)) as usize] as char);
+        }
+        if i + 2 < input.len() {
+            output.push(BASE64_CHARS[(b3 & 0b0011_1111) as usize] as char);
+        }
+
+        i += 3;
+    }
+
+    output
+}
+
+fn base64_encode_using_library<T: AsRef<[u8]>>(input: T) -> String {
+    use base64::engine::general_purpose::STANDARD_NO_PAD;
+    use base64::Engine;
+
+    STANDARD_NO_PAD.encode(input.as_ref())
+}
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_base64_encode() {
+        assert_eq!(base64_encode(&[0b1010_1010, 0b1111_0000, 0b0000_1111]), "qvAP");
+        assert_eq!(base64_encode(b"Hello, World!"), "SGVsbG8sIFdvcmxkIQ");
+    }
+
+    #[test]
+    fn test_base64_encode_using_ibrary() {
+        assert_eq!(base64_encode_using_library(&[0b1010_1010, 0b1111_0000, 0b0000_1111]), "qvAP");
+        assert_eq!(base64_encode_using_library(b"Hello, World!"), "SGVsbG8sIFdvcmxkIQ");
+    }
 
     #[test]
     fn test_encode_bytes() {
