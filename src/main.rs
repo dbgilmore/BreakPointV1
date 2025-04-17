c6f0a6b9532f075ce16cbe4e8c16227f842f8c18
David Gilmore <dgilmore@magnite.com>
2025-04-17 12:01:35 +0100

    obj3, part 2

--- src/main.rs
+++ src/main.rs
@@ -53,9 +53,107 @@ mod tests {
     }
 
     #[test]
-    fn test_base64_encode_using_ibrary() {
+    fn test_base64_encode_using_library() {
         assert_eq!(base64_encode_using_library(&[0b1010_1010, 0b1111_0000, 0b0000_1111]), "qvAP");
         assert_eq!(base64_encode_using_library(b"Hello, World!"), "SGVsbG8sIFdvcmxkIQ");
+    }
+
+    #[test]
+    fn test_base64_encode_empty_input() {
+        assert_eq!(base64_encode(&[]), "");
+        assert_eq!(base64_encode_using_library(&[]), "");
+    }
+
+    #[test]
+    fn test_base64_encode_single_byte() {
+        assert_eq!(base64_encode(&[0b0100_0001]), "QQ");
+        assert_eq!(base64_encode_using_library(&[0b0100_0001]), "QQ");
+    }
+
+    #[test]
+    fn test_base64_encode_two_bytes() {
+        assert_eq!(base64_encode(&[0b0100_0001, 0b0100_0010]), "QUI");
+        assert_eq!(base64_encode_using_library(&[0b0100_0001, 0b0100_0010]), "QUI");
+    }
+
+    #[test]
+    fn test_base64_encode_three_bytes() {
+        assert_eq!(base64_encode(&[0b0100_0001, 0b0100_0010, 0b0100_0011]), "QUJD");
+        assert_eq!(base64_encode_using_library(&[0b0100_0001, 0b0100_0010, 0b0100_0011]), "QUJD");
+    }
+
+    #[test]
+    fn test_base64_encode_non_ascii_characters() {
+        assert_eq!(base64_encode("你好".as_bytes()), "5L2g5aW9");
+        assert_eq!(base64_encode_using_library("你好".as_bytes()), "5L2g5aW9");
+    }
+
+    #[test]
+    fn test_base64_encode_large_input() {
+        let input = vec![0b0100_0001; 1000];
+        let expected = "QQ".repeat(333) + "QQ==";
+        assert_eq!(base64_encode(&input), expected);
+        assert_eq!(base64_encode_using_library(&input), expected);
+    }
+
+    #[test]
+    fn test_base64_encode_all_zero_bytes() {
+        let input = vec![0x00, 0x00, 0x00];
+        assert_eq!(base64_encode(&input), "AAAA");
+        assert_eq!(base64_encode_using_library(&input), "AAAA");
+    }
+
+    #[test]
+    fn test_base64_encode_all_one_bytes() {
+        let input = vec![0xFF, 0xFF, 0xFF];
+        assert_eq!(base64_encode(&input), "////");
+        assert_eq!(base64_encode_using_library(&input), "////");
+    }
+
+    #[test]
+    fn test_base64_encode_mixed_bytes() {
+        let input = vec![0xAB, 0xCD, 0xEF];
+        assert_eq!(base64_encode(&input), "q83v");
+        assert_eq!(base64_encode_using_library(&input), "q83v");
+    }
+
+    #[test]
+    fn test_base64_encode_partial_padding_one_byte() {
+        let input = vec![0b0100_0001];
+        assert_eq!(base64_encode(&input), "QQ");
+        assert_eq!(base64_encode_using_library(&input), "QQ");
+    }
+
+    #[test]
+    fn test_base64_encode_partial_padding_two_bytes() {
+        let input = vec![0b0100_0001, 0b0100_0010];
+        assert_eq!(base64_encode(&input), "QUI");
+        assert_eq!(base64_encode_using_library(&input), "QUI");
+    }
+
+    #[test]
+    fn test_base64_encode_special_characters() {
+        assert_eq!(base64_encode(b"!@#$%^&*()"), "IUAjJCVeJiooKQ");
+        assert_eq!(base64_encode_using_library(b"!@#$%^&*()"), "IUAjJCVeJiooKQ");
+    }
+
+    #[test]
+    fn test_base64_encode_numeric_input() {
+        assert_eq!(base64_encode(b"1234567890"), "MTIzNDU2Nzg5MA");
+        assert_eq!(base64_encode_using_library(b"1234567890"), "MTIzNDU2Nzg5MA");
+    }
+
+    #[test]
+    fn test_base64_encode_alphanumeric_input() {
+        assert_eq!(base64_encode(b"abc123XYZ"), "YWJjMTIzWFla");
+        assert_eq!(base64_encode_using_library(b"abc123XYZ"), "YWJjMTIzWFla");
+    }
+
+    #[test]
+    fn test_base64_encode_long_string() {
+        let input = b"The quick brown fox jumps over the lazy dog.";
+        assert_eq!(base64_encode(input), "VGhlIHF1aWNrIGJyb3duIGZveCBqdW1wcyBvdmVyIHRoZSBsYXp5IGRvZy4");
+        assert_eq!(base64_encode_using_library(input), "VGhlIHF1aWNrIGJyb3duIGZveCBqdW1wcyBvdmVyIHRoZSBsYXp5IGRvZy4");
     }
 
     #[test]
